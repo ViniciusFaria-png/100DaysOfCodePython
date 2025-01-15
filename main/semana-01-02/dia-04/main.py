1a54a62a0f8167f0b2e2b13332865c17a0d29773
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-15 13:32:33 -0300

    feat: Day 4

---
+++ main/semana-01-02/dia-04/main.py
@@ -0,0 +1,12 @@
+print("Input/Output e conversão de tipos")
+# Inputs
+A = input() # esse input vai esperar que algo seja digitado para ser armazenado na variável A
+inteiro = input("Digite um número inteiro: ") # esse input vai esperar que um inteiro seja digitado para ser armazenado na variável inteiro
+string = input("Digite uma string: ") # esse input vai esperar que uma string seja digitada para ser armazenada na variável string
+flutuante = input("Digite um número flutuante: ") # esse input vai esperar que um número flutuante seja digitado para ser armazenado na variável flutuante
+
+# Outputs
+print(A) # imprime o valor da variável A
+print(inteiro) # imprime o valor da variável inteiro
+print(string) # imprime o valor da variável string
+print(flutuante) # imprime o valor da variável flutuante
