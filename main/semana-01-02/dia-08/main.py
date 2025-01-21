b9281653393be12a96288d4f153faa428243c1d7
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-21 15:31:27 -0300

    feat: Day 8

---
+++ main/semana-01-02/dia-08/main.py
@@ -0,0 +1,17 @@
+print("Loops for e range()")
+
+#range(Start, stop, step)
+
+range(10) # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+range(3, 8) # 3, 4, 5, 6, 7
+range(5, 12, 3) # 5, 8, 11
+
+Lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for valor in Lista:
+    print(valor)
+
+
+for i in range(10):
+    print(i)
+
