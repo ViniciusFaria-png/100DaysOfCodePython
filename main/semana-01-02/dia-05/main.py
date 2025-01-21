844c4105febf19248c1118b2fa9821bec6e56f27
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-21 15:17:31 -0300

    feat: Day 5

---
+++ main/semana-01-02/dia-05/main.py
@@ -0,0 +1,53 @@
+print("Operadores (Aritméticos, comparação, lógicos e de atribuição)")
+
+inteiroA = 1
+inteiroB = 2
+
+# Operadores aritméticos
+
+# Adição
+print(inteiroA + inteiroB) # imprime
+# Subtração
+print(inteiroA - inteiroB) # imprime
+# Multiplicação
+print(inteiroA * inteiroB) # imprime
+# Divisão
+print(inteiroA / inteiroB) # imprime
+# Divisão inteira
+print(inteiroA // inteiroB) # imprime
+# Resto da divisão
+print(inteiroA % inteiroB) # imprime
+# Potência
+print(inteiroA ** inteiroB) # imprime
+
+# Operadores de comparação
+print(inteiroA == inteiroB) # verifica se os valores sao iguais e imprime true or false
+print(inteiroA != inteiroB) # verifica se os valores sao diferente e imprime true or false
+print(inteiroA > inteiroB) # verifica se o valor de A é maior que o valor de B e imprime true or false
+print(inteiroA < inteiroB) # verifica se o valor de A é menor que o valor de B e imprime true or false
+print(inteiroA >= inteiroB) # verifica se o valor de A é maior ou igual ao valor de B e imprime true or false
+print(inteiroA <= inteiroB) # verifica se o valor de A é menor ou igual ao valor de B e imprime true or false
+print(inteiroA is inteiroB) # verifica se os valores sao iguais e imprime true or false
+print(inteiroA is not inteiroB) # verifica se os valores sao diferentes e imprime true or false
+
+# Operadores lógicos
+print(inteiroA == 1 and inteiroB == 2) # verifica ambas as condições sao verdadeiras e imprime true or false
+print(inteiroA == 1 or inteiroB == 3) # verifica se pelo menos uma das condições é verdadeira e imprime true or false
+print(not inteiroA == 1) # verifica se a condição é falsa e imprime true or false
+
+# Operadores de atribuição
+inteiroA += 1 # adiciona 1 ao valor de inteiroA
+print(inteiroA) # imprime
+inteiroA -= 1 # subtrai 1 ao valor de inteiroA
+print(inteiroA) # imprime
+inteiroA *= 2 # multiplica o valor de inteiroA por 2
+print(inteiroA) # imprime
+inteiroA /= 2 # divide o valor de inteiroA por 2
+print(inteiroA) # imprime
+inteiroA //= 2 # divide o valor de inteiroA por 2 e arredonda para baixo
+print(inteiroA) # imprime
+inteiroA %= 2 # divide o valor de inteiroA por 2 e retorna o resto
+print(inteiroA) # imprime
+inteiroA **= 2 # eleva o valor de inteiroA a potência de 2
+print(inteiroA) # imprime
+
