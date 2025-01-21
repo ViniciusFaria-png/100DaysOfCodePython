ad3d948b7a836e30aa9211abd6df8486db0721da
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-21 15:23:44 -0300

    feat: Day 6

---
+++ main/semana-01-02/dia-06/main.py
@@ -0,0 +1,11 @@
+print("if, elif, else")
+
+inteiroA = 1
+inteiroB = 2
+
+if inteiroA == inteiroB:
+    print("Os valores são iguais")
+elif inteiroA > inteiroB:
+    print("O valor de A é maior que o valor de B")
+else:
+    print("O valor de A é menor que o valor de B")
