b16f874f71c68d4c179a2704b03400ac892677cd
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-22 14:43:01 -0300

    feat: Day 9 and Day 10

---
+++ main/semana-01-02/dia-09/main.py
@@ -0,0 +1,13 @@
+print("Break e Continue")
+
+# Break: Interrompe o loop
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+# Continue: Pula a iteração atual
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
---
+++ main/semana-01-02/dia-10/main.py
@@ -0,0 +1,6 @@
+print("Nested loops")
+
+# Nested loops são loops dentro de loops
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(f"i: {i}, j: {j}")
