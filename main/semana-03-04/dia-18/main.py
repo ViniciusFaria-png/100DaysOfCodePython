73e3bdd3452f290ab888bfd884f675116f752055
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-23 14:54:51 -0300

    feat: Day 18

---
+++ main/semana-03-04/dia-18/main.py
@@ -0,0 +1,23 @@
+print("Tuplas e operacoes com tuplas")
+
+# Tuplas
+
+# Criando uma tupla vazia
+
+tupla = ()
+
+# Adicionando elementos a tupla
+
+tupla = tupla + (1,)
+tupla = tupla + (2,)
+tupla = tupla + (3,)
+tupla = tupla + (4,)
+tupla = tupla + (5,)
+
+print(tupla)
+
+tupla = tuple(("maca", "banana", "cereja"))
+print(tupla)
+
+
+tupla = ("abc", 34, True)
