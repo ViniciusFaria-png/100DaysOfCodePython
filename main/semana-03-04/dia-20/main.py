1f8fa4e9090260df6ef80ca445a73e60dcda2b70
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-23 14:57:53 -0300

    feat: Day 20

---
+++ main/semana-03-04/dia-20/main.py
@@ -0,0 +1,19 @@
+print("Ordenacao e busca em listas")
+
+# Ordenacao e busca em listas
+
+lista = [50, 10, 235, 1, 99, 5, 111, 6, 7, 0]
+
+print(lista)
+
+lista.sort()
+
+print(lista)
+
+print(lista.index(5))
+
+print(lista.index(10))
+
+print(lista.index(1))
+
+print(lista.index(7))
