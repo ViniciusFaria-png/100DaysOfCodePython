e6ff96f90696db3d8355bb6956e6957260ab06b2
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-23 14:56:25 -0300

    feat: Day 19

---
+++ main/semana-03-04/dia-19/main.py
@@ -0,0 +1,19 @@
+print("Listas Aninhadas")
+
+# Listas Aninhadas
+
+lista = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(lista)
+
+print(lista[0])
+
+print(lista[0][0])
+
+print(lista[1][1])
+
+print(lista[2][2])
+
+print(lista[0][1])
+
+print(lista[1][0])
