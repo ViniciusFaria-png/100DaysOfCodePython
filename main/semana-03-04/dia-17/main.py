23cd409f8367b79fa8a32c513276683fe49962ff
Vinicius Faria <vinicius.f4ria@gmail.com>
2025-01-23 14:49:15 -0300

    feat: Day 17

---
+++ main/semana-03-04/dia-17/main.py
@@ -0,0 +1,21 @@
+print("Slicing")
+
+# Slicing
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(lista[0:5])
+
+print(lista[5:])
+
+print(lista[:5])
+
+print(lista[0:10:2])
+
+print(lista[::2])
+
+print(lista[::-1])
+
+print(lista[9:0:-1])
+
+print(lista[9::-1])
